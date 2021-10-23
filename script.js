cf1b696ffd79c6c9c8326d5aa7fbdd10e52dc591
Charles-LM <clewismangali@gmail.com>
2021-10-23 17:39:12 +1100

    working on winnerCheck

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
 
-const validation = ["rock", "paper", "scissors"];
+//const validation = ["rock", "paper", "scissors"];
+
+const player = document.querySelector("p1");
+const computer = document.querySelector("p2");
+const winner = document.querySelector("h1");
+
 
 
 
@@ -19,47 +24,66 @@ function computerPlay(){
     }
 }
 
-const button = document.querySelectorAll('.buttons');
-for(let i = 0; i < button.length; i++){
-    button[i].addEventListener('click', playRound);
-    
-}
-console.log(button);
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    computer.textContent = `Computer Score: ${computerScore}`;
+    player.textContent = `Player Score: ${playerScore}`;
 
-
-function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
 
         return `It's a draw! Your score: ${playerScore}, Computer score: ${computerScore}`;
-
-        
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
         playerScore++;
-
+        
         return `You win! Rock beats Scissors! Your score: ${playerScore}, Computer score: ${computerScore}`;
-
     }
     else if(playerSelection === 'scissors' && computerSelection === 'rock'){
         computerScore++;
 
         return `Computer wins! Rock beats Scissors! Your score: ${playerScore}, Computer score: ${computerScore}`;
-
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
         playerScore++;
 
         return `You win! Scissors beats Paper! Your score: ${playerScore}, Computer score: ${computerScore}`;
+    }
+    else{
+        computerScore++; 
+
+        return `Computer wins! Scissors beats Paper! Your score: ${playerScore}, Computer score: ${computerScore}`;
+    }
+}
+
+function winnerCheck(playerSelection){
+    if (playerScore === 5){
+       
+    }else if(){
 
     }
     else{
-        computerScore++;
-
-        return `Computer wins! Scissors beats Paper! Your score: ${playerScore}, Computer score: ${computerScore}`;
-
+        console.log(playRound(playerSelection));
     }
 
 }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        winnerCheck(button.value);
+    })
+
+})
+
+
+
+// const button = document.querySelectorAll('.buttons');
+// for(let i = 0; i < button.length; i++){
+//     button[i].addEventListener('click', function(){
+//         playRound(button.value)
+   
+//     });
+// }
 
 
 // function game(){
